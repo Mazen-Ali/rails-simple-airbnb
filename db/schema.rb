ed7d01ca0f2df510b520f94867aeb0e51ff7a7a6
Mazen Ali <mazinjawad2@gmail.com>
2019-11-06 16:18:41 +0100

    DONE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_045353) do
+ActiveRecord::Schema.define(version: 2019_11_06_035353) do
 
   create_table "flats", force: :cascade do |t|
     t.string "name"
